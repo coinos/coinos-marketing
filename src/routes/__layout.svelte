09c76f81837265a32437de2bbc29252a96b57cc7
Adam Soltys <asoltys@gmail.com>
2021-06-16 19:42:27 -0700

    footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,37 @@
 	</div>
 </div>
 
-<slot />
+<main>
+	<div class="mx-auto min-h-screen">
+		<slot />
+	</div>
+</main>
+
+<div class="bg-black px-2 mt-24">
+	<div class="container footer mx-auto flex flex-wrap text-white pt-12">
+		<div class="flex flex-col text-sm w-full lg:w-2/4 mb-10 order-last lg:order-first">
+			<a href="/"><img src="/coinos.svg" alt="logo" class="w-48" /></a>
+			<p class="mt-4">&copy;2021 Coinos</p>
+		</div>
+		<div class="w-full lg:w-1/4 mb-10">
+			<h4 class="font-bold mb-4">Follow Us</h4>
+			<ul class="list-none">
+				<li class="mb-2"><a href="https://twitter.com/coinoswallet">Twitter</a></li>
+				<li class="mb-2"><a href="https://t.me/coinos">Telegram</a></li>
+				<li class="mb-2"><a href="https://coinos.io/blog">Blog</a></li>
+			</ul>
+		</div>
+		<div class="w-full lg:w-1/4 mb-10">
+			<h4 class="font-bold mb-4">Help</h4>
+			<ul class="list-none">
+				<li class="mb-2"><a href="https://t.me/coinos">Support</a></li>
+        <li class="mb-2"><a href="/faq">FAQ</a></li>
+				<li class="mb-2"><a href="/privacy">Privacy Policy</a></li>
+				<li class="mb-2"><a href="/terms">Terms of Service</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
 
 <style global>
 	@tailwind base;
---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<div class="container">
+  <p>We make bitcoin more usable every day. Liquid & Lightning web wallet. The original bitcoin point-of-sale & white label NFT marketplace for businesses & brands.</p>
+</div>
+
