5c9db2fa4bfa5e65d1cc6f6d62256ce9ca52a58d
Adam Soltys <asoltys@gmail.com>
2021-06-16 19:30:44 -0700

    topnav

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,27 @@
-<style global lang="postcss">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+<div class="flex header bg-primary h-24 justify-center border-b-4 border-black">
+	<div class="flex my-auto w-full">
+		<h1 class="text-4xl ml-4 mr-auto">coinos</h1>
+		<div class="flex">
+			<a href="/products">Products</a>
+			<a href="/blog">Blog</a>
+			<a href="/about">About Us</a>
+		</div>
+	</div>
+</div>
 
-  body {
-    font-family: 'Poppins', serif;
-  }
+<slot />
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+	body {
+		font-family: 'Poppins', serif;
+		@apply bg-gray-800;
+	}
+
+	.header a {
+		@apply block mx-4;
+	}
 </style>
-
-<slot></slot>
--- src/routes/index.svelte
+++
@@ -1,6 +0,0 @@
-<div class="flex header text-white w-full bg-black h-24">
-	<img src="/static/coinos.svg" alt="coinos" class="my-auto ml-4" />
-	<a href="/products">Products</a>
-	<a href="/products">Blog</a>
-	<a href="/products">About Us</a>
-</div>
