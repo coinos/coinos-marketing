f3dcc804edd9b60e58cec19ab6c1ee13631b1e3e
Derrick Schwabe <drschwabe@fastmail.com>
2021-10-26 11:19:45 -0600

    Establish Jobs page and text for generic 'we are hiring'

---
+++ src/routes/jobs.svelte
@@ -0,0 +1,45 @@
+<div class="mx-auto max-w-1100px pt-10 pl-10 pr-7 pb-[900px]">
+	<h1 class="mb-10 font-bold">Jobs</h1>
+	<div class="flex mt-8">
+		<div class=" max-w-4xl">
+			
+			<div class="border border-gray-400 p-6 rounded-lg">
+				<h2 class="font-bold">Current Openings</h2>
+				<p>
+					Coinos is actively looking for developers to join our growing team (fulltime or part-time, remote working OK) 
+				</p>  
+	
+				<p>
+					Excited about working on open source, Bitcoin and NFTs?  Then take a look at our <a  class="font-bold" target="_blank"  href="https://github.com/coinos/coinos-server">Bitcoin</a> or <a class="font-bold"  target="_blank" href="https://github.com/liquidnft/lnft">Liquid NFT</a> platform repositories on Github.  
+				</p>
+				
+				<p>
+					If you think you can make an impact, we'd love to hear from you.  Send us your resume to 			<a class="text-lg font-bold hover:text-blue-600" href="mailto:contact@coinos.io">contact@coinos.io</a>
+				</p>
+			</div>
+
+<!-- 
+			<h2 class="mt-20">Bounty Lab</h2>
+
+			<p>
+				Check out the following bounties projects we have made available!   These overlap with our technology and/or short-term objectives so we are pleased to reward satoshis to first person able to fulfill the bounty by meeting all requirements as outlined in the given project.  Go ahead, experiment! 
+			</p> -->
+		</div>
+	</div>
+</div>
+
+<style>
+	p,
+	h1,
+	h2,
+	hr {
+		@apply mb-4;
+	}
+
+	h1 {
+		@apply text-4xl;
+	}
+	h2 {
+		@apply text-2xl;
+	}
+</style>
