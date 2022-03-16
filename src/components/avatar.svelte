857e5db768c09d177e56048eb654ae6249b2dd34
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-03-15 22:04:56 -0600

    Add community page

---
+++ src/components/avatar.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let link = '';
+	export let image = '';
+	export let name = '';
+</script>
+
+<div class="m-4">
+	<a href={link}><img src={image} alt="avatar" class="rounded-full w-32" /></a>
+	<p class="text-center font-medium">{name}</p>
+</div>
